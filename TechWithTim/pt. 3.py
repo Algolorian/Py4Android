2d5054c8fd9f4896899c79333b62539ef815b0c8
Algolorian <93614894+Algolorian@users.noreply.github.com>
2022-06-23 19:47:43 -0500

    Initial commit

---
+++ TechWithTim/pt. 3.py
@@ -0,0 +1,53 @@
+from kivy.app import App
+from kivy.uix.label import Label
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.textinput import TextInput
+from kivy.uix.button import Button
+
+
+class MyGrid(GridLayout):
+    def __init__(self, **kwargs):
+        super(MyGrid, self).__init__(**kwargs)
+        self.cols = 1
+
+        self.inside = GridLayout()
+        self.inside.cols = 2
+
+        # ALL OF THESE ARE APART OF THE (INTERIOR)NEW LAYOUT
+        self.inside.add_widget(Label(text="First Name: "))
+        self.name = TextInput(multiline=False)
+        self.inside.add_widget(self.name)
+
+        self.inside.add_widget(Label(text="Last Name: "))
+        self.lastName = TextInput(multiline=False)
+        self.inside.add_widget(self.lastName)
+
+        self.inside.add_widget(Label(text="Email: "))
+        self.email = TextInput(multiline=False)
+        self.inside.add_widget(self.email)
+
+        self.add_widget(self.inside)
+
+        self.submit = Button(text="Sumbit", font_size=40)
+        self.submit.bind(on_press=self.pressed)
+
+        self.add_widget(self.submit)
+
+    def pressed(self, instance):
+        name = self.name.text
+        last = self.lastName.text
+        email = self.email.text
+
+        print('Name:', name, 'Lastname:', last, 'Email:', email)
+        self.name.text = ""
+        self.lastName.text = ""
+        self.email.text = ""
+
+
+class MyApp(App):
+    def build(self):
+        return MyGrid()
+
+
+if __name__ == "__main__":
+    MyApp().run()
